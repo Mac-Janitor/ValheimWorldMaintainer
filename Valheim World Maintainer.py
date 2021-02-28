f7d4b1502437eb9f7f066b403566518b54fbba8f
Jordan McClintock <jmac3293@gmail.com>
2021-02-28 13:42:06 -0600

    Added ability to download files from github

--- Valheim World Maintainer.py
+++ Valheim World Maintainer.py
@@ -5,21 +5,43 @@ import requests
 with open("config.json") as json_data_file:
     config = json.load(json_data_file)
 
-host = False
-
-def pull_from_github(filename, repo, branch, token):
-    global host
+def determine_host(filename, repo, branch, token):
     url="https://api.github.com/repos/"+repo+"/contents/"+filename
 
     data = requests.get(url+'?ref='+branch, headers = {"Authorization": "token "+token, "Accept": "application/vnd.github.v3.raw"})
 
     textLines = data.text.splitlines()
     if textLines[-1].startswith("Checked In"):
-        host = True
-    print(textLines[-1])
-    if host == True:
-        print("You da host")
-        # download_file(filename, repo, branch, token)
+        return True
+    return False  
+
+def download_world_files(worldName, repo, branch, token, gamePath):
+    url="https://api.github.com/repos/"+repo+"/"
+
+    # Get the sha of the latest commit
+    response = requests.get(url+'git/refs/heads/'+branch, headers = {"Authorization": "token "+token, "Accept": "application/vnd.github.v3.raw"})
+    sha = response.json()["object"]["sha"]
+    print(sha)
+
+    # Get the commit info to get the sha of the tree
+    response = requests.get(url+'git/commits/'+sha, headers = {"Authorization": "token "+token, "Accept": "application/vnd.github.v3.raw"})
+    sha = response.json()["tree"]["sha"]
+    print (sha)
+
+    # Get the contents of the tree
+    response = requests.get(url+'git/trees/'+sha, headers = {"Authorization": "token "+token, "Accept": "application/vnd.github.v3.raw"})
+    files = response.json()["tree"]
+
+    # Copy each world file
+    for file in files:
+        fileName = str(file["path"])
+        if fileName.startswith(worldName):
+            sha = file["sha"]
+            print("Success!")
+            bytes = requests.get(url+'git/blobs/'+sha, headers = {"Authorization": "token "+token, "Accept": "application/vnd.github.v3.raw"}).content
+            f = open(gamePath+"/"+fileName, "wb")
+            f.write(bytes)
+            f.close()
 
 def push_to_github(filename, repo, branch, token):
     url="https://api.github.com/repos/"+repo+"/contents/"+filename
@@ -43,9 +65,18 @@ def push_to_github(filename, repo, branch, token):
         print("nothing to update")
 
 token = config["git"]["token"]
-filename= config["git"]["filename"]
+logFileName= config["git"]["logFileName"]
 repo = config["git"]["repo"]
 branch = config["git"]["branch"]
+worldName = config["valheim"]["worldName"]
+gamePath = config["valheim"]["path"]
 
-pull_from_github(filename, repo, branch, token)
-# push_to_github(filename, repo, branch, token)
+push_to_github(logFileName, repo, branch, token)
+
+host = determine_host(logFileName, repo, branch, token)
+if host == True:
+    print("You da host")
+    download_world_files(worldName, repo, branch, token, gamePath)
+else:
+    print("You NOT da host")
+
